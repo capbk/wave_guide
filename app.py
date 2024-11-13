2d3cc3dc134abed98340dd6471d8aeed61dc9a69
jeffreycapobianco <jeffmcap@gmail.com>
2024-11-12 21:58:13 -0500

    make sure nobody spams the update_server endpoint

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask import Flask, session, request, redirect, render_template, jsonify
 import spotipy
 from werkzeug.exceptions import abort
 import git
+import hmac
+import hashlib
 
 from recommendation_engine import playlist
 from recommendation_engine.mood_track_finder import MoodTrackFinder
@@ -118,16 +120,68 @@ def new_playlist():
 # utility to update pythonanywhere code with latest main branch ===
 # =================================================================
 # https://medium.com/@aadibajpai/deploying-to-pythonanywhere-via-github-6f967956e664
+def is_valid_signature(x_hub_signature, data, private_key):
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
+
 @app.route('/update_server', methods=['POST'])
 def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('/home/waveguide/wave_guide')
-        origin = repo.remotes.origin
-        origin.fetch()
-        origin.pull(refspec='main', progress=None)
-        return 'Updated PythonAnywhere successfully', 200
+    if request.method != 'POST':
+        return 'OK'
     else:
-        return 'Wrong event type', 400
+        abort_code = 418
+        # Do initial validations on required headers
+        if 'X-Github-Event' not in request.headers:
+            abort(abort_code)
+        if 'X-Github-Delivery' not in request.headers:
+            abort(abort_code)
+        if 'X-Hub-Signature' not in request.headers:
+            abort(abort_code)
+        if not request.is_json:
+            abort(abort_code)
+        if 'User-Agent' not in request.headers:
+            abort(abort_code)
+        ua = request.headers.get('User-Agent')
+        if not ua.startswith('GitHub-Hookshot/'):
+            abort(abort_code)
+
+        event = request.headers.get('X-GitHub-Event')
+        if event == "ping":
+            return json.dumps({'msg': 'Hi!'})
+        if event != "push":
+            return json.dumps({'msg': "Wrong event type"})
+
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+        if not is_valid_signature(x_hub_signature, request.data, w_secret):
+            print('Deploy signature failed: {sig}'.format(sig=x_hub_signature))
+            abort(abort_code)
+
+        payload = request.get_json()
+        if payload is None:
+            print('Deploy payload is empty: {payload}'.format(
+                payload=payload))
+            abort(abort_code)
+
+        if payload['ref'] != 'refs/heads/pythonanywhere_deployment':  # Changed to match your branch
+            return json.dumps({'msg': 'Not pythonanywhere_deployment branch; ignoring'})
+
+        repo = git.Repo('/home/waveguide/wave_guide')  # Using your repo path
+        origin = repo.remotes.origin
+
+        pull_info = origin.pull(refspec='pythonanywhere_deployment', progress=None)
+
+        if len(pull_info) == 0:
+            return json.dumps({'msg': "Didn't pull any information from remote!"})
+        if pull_info[0].flags > 128:
+            return json.dumps({'msg': "Didn't pull any information from remote!"})
+
+        commit_hash = pull_info[0].commit.hexsha
+        build_commit = f'build_commit = "{commit_hash}"'
+        print(f'{build_commit}')
+        return 'Updated PythonAnywhere server to commit {commit}'.format(commit=commit_hash)
 
 # Test utility to experiment with feature paramaters =================================
 # ====================================================================================
